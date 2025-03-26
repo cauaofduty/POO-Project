b58eb88d5536d70cf265b3bd973e6eeb6cf0f336
caua de SOUZA <cauasantos064@gmail.com>
2025-03-26 16:52:06 -0300

    testando commits
    Não sei exatamente o que estou a fazer

---
+++ UBER/src/FormaDePagamento.java
@@ -0,0 +1,6 @@
+public interface FormaDePagamento {
+    public void debitar();
+}
+
+
+
---
+++ UBER/src/Pessoa.java
@@ -0,0 +1,29 @@
+public abstract class Pessoa {
+    protected String nome;
+    protected String telefone;
+    protected int IDPessoa;
+
+        public Pessoa(String nome,String telefone,int IDPessoa){//atributos default para pessoa
+            this.nome = nome;
+            this.telefone = telefone;
+            this.IDPessoa = IDPessoa;
+        }
+        //getters default abaixo
+        public String getNome(){
+            return this.nome;
+        }
+
+        public String getTelefone(){
+            return this.telefone;
+        }
+        
+        public int getIDPessoa(){
+            return this.IDPessoa;
+        }
+    /* Classe	O que usar?	Justificativa
+Pessoa	Classe Abstrata	Cliente e Motorista compartilham atributos, mas têm lógica diferente.
+FormaDePagamento	Interface	Cada tipo de pagamento tem implementação própria.
+Local	Classe Normal	Apenas representa um endereço, sem diferenças na lógica.
+Viagem	Classe Abstrata	Viagens compartilham estrutura, mas cálculos podem ser diferentes.
+Veiculo	Classe Abstrata	Compartilham atributos, mas têm lógica de tarifa diferente. */
+}
