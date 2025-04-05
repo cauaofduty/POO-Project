285e40269cfd447a4cf4064aab95bcc13eeb552d
Hugo Costa <hugocosta535@gmail.com>
2025-04-05 05:23:42 -0300

    Classe viagem alterada.

--- UBER/src/negocio/localizacao/ViagemEntrega.java
+++ UBER/src/negocio/localizacao/ViagemEntrega.java
@@ -1,23 +1,24 @@
 package negocio.localizacao;
-import negocio.Cliente;
-import negocio.Motorista;
-import negocio.modelos.Viagem;
 
-public class ViagemEntrega extends Viagem{
-    private double pesoEntrega;//implementar tempo nas classes, perguntar ao professor
+import negocio.pessoas.Cliente;
+import negocio.pessoas.Motorista;
 
+public class ViagemEntrega extends Viagem {
+    private double pesoEntrega;
 
-    public ViagemEntrega(double pesoEntrega, Local origem, Local destino, Cliente cliente, Motorista motorista) {
-        super(origem, destino, cliente, motorista);
+    public ViagemEntrega(Local origem, Local destino, Cliente cliente, Motorista motorista, double preco, double pesoEntrega) {
+        super(origem, destino, cliente, motorista, preco);
         this.pesoEntrega = pesoEntrega;
     }
     
     public double getPesoEntrega() {
         return pesoEntrega;
     }
-
-
+    
     public void setPesoEntrega(double pesoEntrega) {
+        if (pesoEntrega < 0) throw new IllegalArgumentException("Peso da entrega não pode ser negativo.");
         this.pesoEntrega = pesoEntrega;
     }
+
+    // Adicionar toString() para a classe ViagemEntrega, se necessário
 }
