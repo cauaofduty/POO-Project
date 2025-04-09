be1a9cdd79847ec5204d994296e4f14a8693ac22
Hugo Costa <hugocosta535@gmail.com>
2025-04-08 23:51:42 -0300

    Atualizações das classes de Repositorio

--- UBER/src/dados/IRepositorioPessoa.java
+++ UBER/src/dados/IRepositorioPessoa.java
@@ -2,14 +2,13 @@ package dados;
 
 import java.util.List;
 
-import negocio.exceptions.PessoaNaoEncontradaException;
 import negocio.pessoas.Cliente;
 import negocio.pessoas.Motorista;
 import negocio.pessoas.Pessoa;
 
 public interface IRepositorioPessoa <P extends Pessoa> {
     void adicionar(Pessoa pessoa);//mesmo efeito
-    Pessoa buscarPorID(String IDPessoa) throws PessoaNaoEncontradaException;//mudei para mesmo nome do atributo
+    Pessoa buscarPorID(String IDPessoa);
     List<Cliente> listarClientes();
     List<Motorista> listarMotoristas();
 }
