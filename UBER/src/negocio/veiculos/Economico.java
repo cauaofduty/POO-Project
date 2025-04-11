2e378f521fb9620203bc62497f0a9a99d1be54c3
Hugo Costa <hugocosta535@gmail.com>
2025-04-11 19:20:18 -0300

    Organização das classes do pacote de veiculos

--- UBER/src/negocio/veiculos/Economico.java
+++ UBER/src/negocio/veiculos/Economico.java
@@ -3,30 +3,25 @@ package negocio.veiculos;
 public class Economico extends Veiculo {
     private static final long serialVersionUID = 200004L;
     private final double taxaViagem = 0;
-    private final String tipo = "economico";//caso necessite identificar o tipo de veiculo, pode ser utilizado no repositorio de veiculos
+    private final String tipo = "economico"; //caso necessite identificar o tipo de veiculo, pode ser utilizado no repositorio de veiculos
 
     
-
     public Economico(String placa, String cor, String nomeVeiculo, int ano) {
         super(placa, cor, nomeVeiculo, ano);                   
     }
+
 
     @Override
     public double getTaxaViagem() {
         return taxaViagem;
     }
 
-    @Override//VER NO QUE ESTE OVERRIDE DA
-    public String toString(){
-        return "Economico {" + super.toString()  + this.getTaxaViagem() +"}";
-    }
-
     public String getTipo() {
         return tipo;
     }
 
-   
-    
-
+    public String toString() {
+        return "Economico {" + super.toString()  + this.getTaxaViagem() +"}";
+    }
     
 }
